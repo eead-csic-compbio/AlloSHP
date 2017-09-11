8adc98e67651d05f406a82e9f2668cb5ed2ba59a
eead-csic-compbio <compbio@eead.csic.es>
2017-09-11 16:52:58 +0200

    added GZIP/BZIP2 support

--- _rm_double_lines.pl
+++ _rm_double_lines.pl
@@ -1,7 +1,41 @@
 #!/usr/bin/perl -w
 use strict;
 
-open(FH,"zcat $ARGV[0] |"); #){ die "# usage: $0 <file.vcf.gz>\n" }
+# removes repeated VCF lines with indels, if any
+
+# Input1: VCF file, which might be GZIP/BZIP2-compressed
+
+# Bruno Contreras, Ruben Sancho EEAD-CSIC 2017
+
+my $infile;
+if($ARGV[0]){ $infile = $ARGV[0] }
+else{ die "# usage: $0 <file.vcf[.gz|.bz2]>\n" }
+
+# check input file format and open it accordingly
+my $magic;
+open(INFILE,$infile) || die "# cannot read $infile, exit\n";
+sysread(INFILE,$magic,2);
+close(INFILE);
+
+if($infile =~ /\.gz$/ || $magic eq "\x1f\x8b") # GZIP compressed input
+{
+  if(!open(FH,"gzip -dc $infile |"))
+  {
+    die "# cannot read GZIP compressed $infile $!\n"
+      ."# please check gzip is installed\n";
+  }
+}
+elsif($infile =~ /\.bz2$/ || $magic eq "BZ") # BZIP2 compressed input
+{
+  if(!open(FH,"bzip2 -dc $infile |"))
+  {
+    die "# cannot read BZIP2 compressed $infile $!\n"
+      ."# please check bzip2 is installed\n";
+  }
+}
+else{ open(FH,"<",$infile) || die "# cannot read $infile $!\n"; }
+
+
 my $prev_line = '';
 while(my $this_line = <FH>) 
 {
