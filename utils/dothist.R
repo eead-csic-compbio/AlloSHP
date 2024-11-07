1602f4262e55df52fc7d8b3f1306e4b526ee2def
eead-csic-compbio <bcontreras@eead.csic.es>
2024-11-07 12:07:05 +0100

    improved

--- utils/dothist.R
+++ utils/dothist.R
@@ -1,16 +1,29 @@
 
+# example call, generates file Rplots.pdf: 
+# Rscript utils/dothist.R Bdis.fna.gz.Bsta.fna.gz_Cgaln_-K11_-BS10000_-X12000_-fc_-cons.dot Bdis.fna.gz.Bsta.fna.gz_GSAlign_-no_vcf_-one.dot
+
 library(stringr)
 
 args <- commandArgs(TRUE)
 
-dotfile1 <- readLines(args[1]) #"Bdis.fna.gz.Bsta.fna.gz_GSAlign_-no_vcf_-one.dot")
-dotfile2 <- readLines(args[2]) #"Bdis.fna.gz.Bsta.fna.gz_Cgaln_-K11_-BS10000_-X12000_-fc_-cons.dot")
+#dotfile1 <- readLines("../Bdis.fna.gz.Bsta.fna.gz_GSAlign_-no_vcf_-one.dot")
+#dotfile2 <- readLines("../Bdis.fna.gz.Bsta.fna.gz_Cgaln_-K11_-BS10000_-X12000_-fc_-cons.dot")
+dotfile1 <- readLines(args[1]) 
+dotfile2 <- readLines(args[2]) 
 
 len1 <- str_match(dotfile1, 'length: (\\d+)')
-hist1 <- hist(as.numeric(len1[,2]))
+len1 <- na.omit(as.numeric(len1[,2]))
+maxlen1 = max(len1) + 200
 
 len2 <- str_match(dotfile2, 'length: (\\d+)')
-hist2 <- hist(as.numeric(len2[,2]))
+len2 <- na.omit(as.numeric(len2[,2]))
+maxlen2 = max(len2) + 200
+
+maxlen = max(maxlen1,maxlen2)
+hist1 <- hist(len1, breaks = seq(from=1, to=maxlen, by=200))
+hist2 <- hist(len2, breaks = seq(from=1, to=maxlen, by=200))
+
+
 maxf <- max(hist1$counts,hist2$counts)
 
 col1 = rgb(0,0,1,1/4)
